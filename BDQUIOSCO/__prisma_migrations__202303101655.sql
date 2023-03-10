cf57f04ce67cf932a48b5ce4db20e7f47ccc8472
Fabrizzio Lo Presti <89172392+FabrizzioLoPresti@users.noreply.github.com>
2023-03-10 16:58:41 -0300

    Add files via upload

---
+++ BDQUIOSCO/__prisma_migrations__202303101655.sql
@@ -0,0 +1,4 @@
+INSERT INTO quioscoapp.`_prisma_migrations` (id,checksum,finished_at,migration_name,logs,rolled_back_at,started_at,applied_steps_count) VALUES
+	 ('1666bd66-7a27-44d0-9771-68c65d01b617','a95dec0fde2a69dae5501ef366105fe918bac2bba73a79a3418d17b6f891c38a','2023-03-08 12:57:16.014','20230308125715_ordenes',NULL,NULL,'2023-03-08 12:57:15.748',1),
+	 ('1c3474ce-00f5-418c-8830-a024bb6c6a90','ec9a5b675dd855d209c09fffe4ea363d6d442a47f419628c91285db5c3265aae','2023-03-10 16:47:25.668','20230310164725_agregar_estado',NULL,NULL,'2023-03-10 16:47:25.505',1),
+	 ('c0882d74-6873-4ede-a868-652a4c7930ec','4341a488b28d0fa2fbddee0a25204e68af11b8097898855835284285b6d3ed73','2023-03-08 12:45:48.657','20230308124547_categorias_y_productos',NULL,NULL,'2023-03-08 12:45:47.617',1);
